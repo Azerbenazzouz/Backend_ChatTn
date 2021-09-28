70e0d6efba5071c6ebae14d1947ca534f36a3b67
azer <benazzouzaze@gmail.com>
2021-09-28 07:36:46 +0100

    ❤Backend Chat Tn🌌

--- controles/Token/Token.js
+++ controles/Token/Token.js
@@ -4,30 +4,35 @@ require('dotenv').config();
 
 
 const Token=async (req,res)=>{
-    console.log(req.body)
+    //* Get the refresh token
     const refreshToken = req.body.token
 
-    // check if the refresh token is valid
+    //* Check if the refresh token is not null
     if(refreshToken == null) return sendStatus(299)
-
+    //* Search for the refresh token in the database
     RefreshTokens.find().then(async(val)=>{
         // if(val==null) return res.sendStatus(299)
+        //* Create local refresh token array 
         refreshTokens=[]
+        //* Add the refresh token to the array
         val.map((value)=>{
             refreshTokens.push(value.refreshTokens)
         })
+        //* Check if the refresh token is included in the refresh token array
         if (!refreshTokens.includes(refreshToken)) return res.sendStatus(299)
         
-        // verify if the refresh token is valid with the refresh token secret
+        //* Verify if the refresh token is valid with the refresh token secret
         jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET,(err, email)=>{
+            //* if error send status 299
             if ( err ) return res.sendStatus(299)
+            //* Generate New Access Token
             const accessToken= generateAccessToken({email : email.email.email})
+            //* Send the accessToken
             res.json({accessToken : accessToken})
         })
     })
-
-    // res.send(refreshToken)
 }
+//* Generate New Access Token
 const generateAccessToken=(email)=> {return jwt.sign(email , process.env.ACCESS_TOKEN_SECRET ,{ expiresIn: "2592000s"})}
 
 module.exports = Token;
