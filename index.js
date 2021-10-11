787178d4d2766ab02c2b6998b4ffa1ed34d165dd
azer <benazzouzaze@gmail.com>
2021-10-11 12:56:00 +0100

    ❤Backend Chat Tn🌌

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use((req, res, next)=>{
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 
 app.get('/', (req, res) => {
