00e0b9ec8f306ea098698959ae65cfec19c2dbf6
azer <benazzouzaze@gmail.com>
2021-10-15 13:06:46 +0100

    ❤Backend Chat Tn🌌

--- controles/Users/GetUser.js
+++ controles/Users/GetUser.js
@@ -1,8 +1,8 @@
 const { User  } = require('../../models/User');
 
-const GetAll=async (req,res)=>{
+const GetUser=async (req,res)=>{
     await User.findOne(res.email).then((result)=>res.status(200).send(result))
     .catch((err)=>res.status(299).send(err))
 }
 
-module.exports = GetAll;
+module.exports = GetUser;
--- controles/Users/UserController.js
+++ controles/Users/UserController.js
@@ -2,6 +2,6 @@ const Regester = require('./Regester')
 const GetAll = require('./GetAll')
 const Login = require('./Login')
 const Logout = require('./Logout')
+const GetUser = require('./GetUser')
 
-
-module.exports={Regester,GetAll,Login,Logout}
+module.exports={Regester,GetAll,Login,Logout,GetUser}
