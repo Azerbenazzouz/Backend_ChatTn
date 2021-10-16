166e4d5448b3c236cfb02366880ead6c7490caef
azer <benazzouzaze@gmail.com>
2021-10-16 12:49:41 +0100

    ❤Backend Chat Tn🌌

--- controles/Users/GetNames.js
+++ controles/Users/GetNames.js
@@ -1,7 +1,7 @@
 const { User } = require('../../models/User');
 
 const GetNames=async (req,res)=>{
-    await User.find({"username":req.body.username}).then((result)=>res.status(200).send(result))
+    await User.find({"username":req.username}).then((result)=>res.status(200).send(result))
     .catch((err)=>res.status(299).send(err))
 }
 
