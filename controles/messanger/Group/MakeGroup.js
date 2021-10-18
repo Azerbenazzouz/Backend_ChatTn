1123a4a8f5656ca2b6eb5d20d3721dbc7ab8ce96
azer <benazzouzaze@gmail.com>
2021-10-18 19:20:00 +0100

    ❤Backend Chat Tn🌌

--- controles/messanger/Group/MakeGroup.js
+++ controles/messanger/Group/MakeGroup.js
@@ -1,7 +1,7 @@
 const { Group , createGroup } =require('../../../models/Group')
 const MakeGroup=async (req,res)=>{
     const {error , value} =createGroup({...req.body,lastTime:Date.now()})
-    if (error) return res.status(266).send(error)
+    if (error) return res.status(299).send(error)
     const result = await new Group(value).save()
     res.send(result)
 }
