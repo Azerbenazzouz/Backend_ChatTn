748622a5482e1b49df0e6830ce90481e9cb309cb
azer <benazzouzaze@gmail.com>
2021-10-19 13:38:43 +0100

    ❤Backend Chat Tn🌌

--- controles/messanger/Group/GetAllGroups.js
+++ controles/messanger/Group/GetAllGroups.js
@@ -3,7 +3,7 @@ const GetAllGroups=async (req,res)=>{
     Group.find().then((group)=>{
         var data = []
         group.map(grp=>{
-            if(grp.usersEmail.includes(req.body.email)) data.push(grp)
+            if(grp.usersEmail.includes(req.email)) data.push(grp)
         })
         
         res.send(data.reverse(this.lastTime))
